7db7497e028d68a5a412e0a44d5da889c19417a7
irfanripat <irfanripat@gmail.com>
2021-04-18 15:06:54 +0800

    Initial commit

---
+++ app/src/main/java/com/irfan/moviecatalogue/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.irfan.moviecatalogue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
